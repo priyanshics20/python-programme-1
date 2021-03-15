c6fc497605cd955d936a1510ba221b37c182d934
priyanshics20 <80679552+priyanshics20@users.noreply.github.com>
2021-03-15 02:54:21 -0700

    to find upper lower alphabets digits and special character

---
+++ char_id.py
@@ -0,0 +1,10 @@
+n= (input())
+if n >= 'A' and n<='Z' :
+    print("upper case alphabet")
+elif n>='a' and n<='z' :
+    print("lower case alphabet")
+    
+elif n>='0' and n<='9' :
+     print("digits")
+     else :
+         print("special character")
