12b2d6c99c330bb8a9a98f829892aa9eb5f00169
priyanshics20 <80679552+priyanshics20@users.noreply.github.com>
2021-04-12 17:43:03 +0530

    python program to remove word from a string

---
+++ Remove “b” and “ac” from a given string.py
@@ -0,0 +1,5 @@
+#Remove “b” and “ac” from a given string.
+n = input('Enter a string :- ')
+res_str = n.replace('b','')
+res_str2 = res_str.replace('ac','')
+print('The',n,'string after removing "b"and "ac" :- ', res_str2)
